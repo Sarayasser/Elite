95bf1ce4d5a5043c13114797d0aa46fe4f8e3c96
alhaithamkamal <alhaithamkamalali@gmail.com>
2020-05-27 18:19:14 +0200

    new admin template

--- app/User.php
+++ app/User.php
@@ -5,10 +5,19 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+// use Spatie\Permission\Traits\HasRoles, trait;
+use Backpack\CRUD\app\Models\Traits\CrudTrait; // <------------------------------- this one
+use Spatie\Permission\Traits\HasRoles;// <---------------------- and this one
+use Illuminate\Support\Str;
+
+
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use CrudTrait; // <----- this
+    use HasRoles; // <------ and this
+
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','age', 'address', 'phone_number', 'gender', 'image'
     ];
 
     /**
@@ -36,4 +45,33 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function SetImagesAttribute($value)
+    {
+        $attribute_name = "images";
+        $disk = "uploads";
+        $destination_path = "vector/upload/";
+    
+        // if the image was erased
+        if ($value==null) {
+            // delete the image from disk
+            \Storage::disk($disk)->delete($this->{$attribute_name});
+    
+            // set null in the database column
+            $this->attributes[$attribute_name] = null;
+        }
+    
+        // if a base64 was sent, store it in the db
+        if (starts_with($value, 'data:image'))
+        {
+            // 0. Make the image
+            $image = \Image::make($value);
+            // 1. Generate a filename.
+            $filename = md5($value.time()).'.jpg';
+            // 2. Store the image on disk.
+            \Storage::disk($disk)->put($destination_path.'/'.$filename, $image->stream());
+            // 3. Save the path to the database
+            $this->attributes[$attribute_name] = /*$destination_path.'/'.*/$filename;
+        }
+    }
+        
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -47,13 +47,19 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
-
-        'public' => [
+        'uploads' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
+            'root' =>public_path(),
+             'url' => '/vector/upload',
+             'visibility' => 'public',
         ],
+
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        // ],
 
         's3' => [
             'driver' => 's3',
