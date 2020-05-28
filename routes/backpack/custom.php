bbe0f9af133be14fb7ff8574b5ac4ab601c8db66
alhaithamkamal <alhaithamkamalali@gmail.com>
2020-05-28 02:34:15 +0200

    backpack users customized

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,6 +6,8 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
+use Illuminate\Support\Facades\Route;
+
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => [
@@ -15,4 +17,6 @@ Route::group([
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
     Route::crud('tag', 'TagCrudController');
+    Route::crud('user', 'UserCrudController');
+    
 }); // this should be the absolute last line of this file
