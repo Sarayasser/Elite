feb9812b59fbbd664cd8a11657aa371b14c75cd5
alhaithamkamal <alhaithamkamalali@gmail.com>
2020-06-05 15:31:38 +0200

    show validation errors

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container mt-50 mb-50">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
     @csrf
     <div class="form-group">
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 <div class="container mt-50 mb-50">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <form method="POST" action="{{ route('posts.update', ['post' => $post->id]) }}" enctype="multipart/form-data">
     @csrf
     @method('PUT')
