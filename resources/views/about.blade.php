40f6165166e840a7b488b893f8c750a681af3cba
logain hassan <logeenhassanmostafa96@gmail.com>
2020-06-10 20:11:11 +0200

    add owners into about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,9 +11,8 @@
                 <div class="col-md-6">
                 <h2 class="text-theme-color-yellow font-36">About</h2>
                 <ol class="breadcrumb text-left mt-10 white">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Pages</a></li>
-                    <li class="active">About</li>
+                <li><a href="{{route('home')}}">Home</a></li>
+                <li class="active">About</li>
                 </ol>
                 </div>
             </div>
@@ -54,7 +53,7 @@
         <div class="section-title text-center">
           <div class="row">
             <div class="col-md-8 col-md-offset-2">
-              <h2 class="line-bottom-center mt-0">Our <span class="text-theme-color-red">Features</span></h2>
+              <h2 class="line-bottom-center mt-0">Our <span class="text-theme-color-red">Owners</span></h2>
               <div class="title-flaticon">
                 <i class="flaticon-charity-alms"></i>
               </div>
@@ -64,105 +63,88 @@
         </div>
         <div class="section-content">     
           <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-4">              
-              <div class="row features-style1">
-                <div class="col-sm-12">
-                  <div class="icon-box left media p-0 mb-sm-10 mt-30 mt-sm-0"> <a href="#" class="icon icon-circled icon-lg border-1px bg-theme-color-red pull-right flip mr-0 ml-30"><i class="fa fa-pencil-square-o text-white"></i></a>
-                    <div class="media-body text-right">
-                      <h4 class="text-theme-color-blue">Active Learning</h4>
-                      <p>Kidspro is a creative skill and a joy beyond anything found</p>
-                    </div>
-                  </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 sm-text-center mb-sm-15">
+              <div class="team-member maxwidth400">
+                <div class="team-thumb">
+                    <img class="img-fullwidth mt-15" height="390px" src="{{asset('images/team/team2.jpg')}}" alt="">                     
+                </a>
                 </div>
-                <div class="col-sm-12">
-                  <div class="icon-box left media p-0 mb-sm-10"> <a href="#" class="icon icon-circled icon-lg border-1px bg-theme-color-yellow  pull-right flip mr-0 ml-30"><i class="fa fa-book text-white"></i></a>
-                    <div class="media-body text-right">
-                      <h4 class="text-theme-color-sky">Multimedia Class</h4>
-                      <p>Kidspro is a creative skill and a joy beyond anything found</p>
-                    </div>
+                <div class="team-details bg-theme-color-yellow text-center pt-20 pb-5">
+                  <div class="member-biography">
+                    <h3 class="mt-0 text-white">Ahmed Gaber</h3>
+                    <p class="mb-0 text-white">Web Developer</p>
                   </div>
-                </div>
-                <div class="col-sm-12">
-                  <div class="icon-box left media p-0 mb-sm-10"> <a href="#" class="icon icon-circled icon-lg border-1px bg-theme-color-lemon  pull-right flip mr-0 ml-30"><i class="fa fa-graduation-cap text-white"></i></a>
-                    <div class="media-body text-right">
-                      <h4 class="text-theme-color-red">Full Day Programs</h4>
-                      <p>Kidspro is a creative skill and a joy beyond anything found</p>
-                    </div>
-                  </div>
+                  <ul class="styled-icons icon-dark icon-circled icon-theme-color-red pt-5">
+                    <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github text-white"></i></a></li>
+                  </ul>
                 </div>
               </div>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">
-              <img src="{{ asset('images/about/a1.png') }}" alt="">
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-4">              
-              <div class="row features-style1">
-                <div class="col-sm-12">
-                  <div class="icon-box left media p-0 mb-sm-10 mt-30 mt-sm-30"> <a href="#" class="icon icon-circled icon-lg border-1px bg-theme-color-red  pull-left flip"><i class="fa fa-graduation-cap text-white"></i></a>
-                    <div class="media-body">
-                      <h4 class="text-theme-color-blue">Expert Teachers</h4>
-                      <p>Kidspro is a creative skill and a joy beyond anything found</p>
-                    </div>
-                  </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 sm-text-center mb-sm-15">
+              <div class="team-member maxwidth400">
+                <div class="team-thumb">
+                    <img class="img-fullwidth mt-15" height="390px" src="{{asset('images/team/team2.jpg')}}" alt="">                     
+                </a>
                 </div>
-                <div class="col-sm-12">
-                  <div class="icon-box left media p-0 mb-sm-10"> <a href="#" class="icon icon-circled icon-lg border-1px bg-theme-color-yellow pull-left flip"><i class="fa fa-book text-white"></i></a>
-                    <div class="media-body">
-                      <h4 class="text-theme-color-sky">Funny and Happy</h4>
-                      <p>Kidspro is a creative skill and a joy beyond anything found</p>
-                    </div>
+                <div class="team-details bg-theme-color-sky text-center pt-20 pb-5">
+                  <div class="member-biography">
+                    <h3 class="mt-0 text-white">Al Haitham</h3>
+                    <p class="mb-0 text-white">Web Developer</p>
                   </div>
-                </div>
-                <div class="col-sm-12">
-                  <div class="icon-box left media p-0 mb-sm-10"> <a href="#" class="icon icon-circled icon-lg border-1px bg-theme-color-lemon pull-left flip"><i class="fa fa-pencil-square-o text-white"></i></a>
-                    <div class="media-body">
-                      <h4 class="text-theme-color-red">Fulfill With Love</h4>
-                      <p>Kidspro is a creative skill and a joy beyond anything found</p>
-                    </div>
-                  </div>
+                  <ul class="styled-icons icon-dark icon-circled icon-theme-color-red pt-5">
+                    <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github text-white"></i></a></li>
+                  </ul>
                 </div>
               </div>
             </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 sm-text-center mb-sm-15">
+              <div class="team-member maxwidth400">
+                <div class="team-thumb">
+                    <img class="img-fullwidth mt-15" height="390px" src="{{asset('images/team/team1.jpg')}}" alt="">                     
+                </a>
+                </div>
+                <div class="team-details bg-theme-color-red text-center pt-20 pb-5">
+                  <div class="member-biography">
+                    <h3 class="mt-0 text-white">Sara Yasser</h3>
+                    <p class="mb-0 text-white">Web Developer</p>
+                  </div>
+                  <ul class="styled-icons icon-dark icon-circled icon-theme-color-red pt-5">
+                    <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github text-white"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 sm-text-center mb-sm-15">
+              <div class="team-member maxwidth400">
+                <div class="team-thumb">
+                    <img class="img-fullwidth mt-15" height="390px" src="{{asset('images/team/team1.jpg')}}" alt="">                     
+                </a>
+                </div>
+                <div class="team-details bg-theme-color-green text-center pt-20 pb-5">
+                  <div class="member-biography">
+                    <h3 class="mt-0 text-white">Logain hassan</h3>
+                    <p class="mb-0 text-white">Web Developer</p>
+                  </div>
+                  <ul class="styled-icons icon-dark icon-circled icon-theme-color-red pt-5">
+                    <li><a href="#"><i class="fa fa-facebook text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin text-white"></i></a></li>
+                    <li><a href="#"><i class="fa fa-github text-white"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+           
           </div>     
         </div>
       </div>
     </section>
 
-    <!-- Divider: Funfact -->
-    <section class="divider" data-bg-img="{{ asset('images/bg/p5.jpg') }}">
-      <div class="container pt-90 pb-90">
-        <div class="row">
-          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
-            <div class="funfact text-center">
-              <i class="pe-7s-users mt-5 text-white"></i>
-              <h2 data-animation-duration="2000" data-value="864" class="animate-number text-white font-42 font-weight-300 mt-0 mb-0">0</h2>
-              <h5 class="text-white">Qualified Teachers</h5>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
-            <div class="funfact text-center">
-              <i class="pe-7s-study mt-5 text-white"></i>
-              <h2 data-animation-duration="2000" data-value="486" class="animate-number text-white font-42 font-weight-300 mt-0 mb-0">0</h2>
-              <h5 class="text-white">Successful Kids</h5>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
-            <div class="funfact text-center">
-              <i class="pe-7s-smile mt-5 text-white"></i>
-              <h2 data-animation-duration="2000" data-value="1468" class="animate-number text-white font-42 font-weight-300 mt-0 mb-0">0</h2>
-              <h5 class="text-white">Happy Parents</h5>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
-            <div class="funfact text-center">
-              <i class="pe-7s-medal mt-5 text-white"></i>
-              <h2 data-animation-duration="2000" data-value="32" class="animate-number text-white font-42 font-weight-300 mt-0 mb-0">0</h2>
-              <h5 class="text-white">Award Won</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
 
     <!-- Section: Courses -->
     <section data-bg-img="{{ asset('images/bg/p2.jpg') }}" >
@@ -312,27 +294,5 @@
       </div>
     </section>
 
-    <!-- Divider: Clients -->
-    <section class="clients bg-theme-color-sky">
-      <div class="container pt-10 pb-0">
-        <div class="row">
-          <div class="col-md-12">
-            <!-- Section: Clients -->
-            <div class="owl-carousel-6col clients-logo transparent text-center owl-nav-top">
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w1.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w2.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w3.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w4.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w5.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w6.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w3.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w4.png') }}"alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w5.png') }}" alt=""></a></div>
-              <div class="item"> <a href="#"><img src="{{ asset('images/clients/w6.png') }}" alt=""></a></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
 
 @endsection
