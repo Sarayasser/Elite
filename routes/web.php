1c2da0d8b9b5de84a29be7b197f292a8d8e1c04b
logain hassan <logeenhassanmostafa96@gmail.com>
2020-06-11 10:01:36 +0200

    make middleware for auth,verify and role

--- routes/web.php
+++ routes/web.php
@@ -19,60 +19,28 @@ use App\Gamify\Points\PostCompleted;
 */
 
 // Courses
-Route::get('/courses', function () {return view('courses_list',['courses'=>Course::all()]);})->name('courses.index');
-Route::get('/courses/{course}', function () {
-    return view('course_details',[
-
-        'course'=>Course::find(request()->course),'courses'=>Course::all(),'posts'=>Post::all()]);
-})->name('courses.show');
+Route::get('/courses','CourseController@index')->name('courses.index');
+Route::get('/courses/{course}','CourseController@show')->name('courses.show');
 
 Route::get('/about', function () { 
     return view('about', [
         'course'=>Course::find(request()->course),'courses'=>Course::all(),'posts'=>Post::all()]);
      })->name('about');
 
-Route::post('/courses/{course}/enroll', function(Course $course){
-    $user = auth()->user();
-    if($user->hasRole('student'))
-        if ($course->capacity > 0) {
-            $user->courses()->attach($course);
-            $course->capacity --;
-            $course->save();
-        }
-    if(request()->ajax()) // This is check ajax request
-        return response()->json(['enrolled' => 'enrolled']);
-    else
-        return redirect()->route('courses.show', $course->id);
-    
-})->name('courses.enroll');
 
 
 // Posts
 Route::get('/courses/{course}/posts', 'PostController@index')->name('posts.index');
-Route::get('/courses/{course}/posts/create', 'PostController@create')->name('posts.create');
-Route::post('/courses/{course}/posts', 'PostController@store')->name('posts.store');
 Route::get('/courses/{course}/posts/{post}', 'PostController@show')->name('posts.show');
-Route::get('/courses/{course}/posts/{post}/edit', 'PostController@edit')->name('posts.edit');
-Route::put('/courses/{course}/posts/{post}', 'PostController@update')->name('posts.update');
-Route::delete('/courses/{course}/posts/{post}', 'PostController@destroy')->name('posts.destroy');
-
-Route::post('/courses/{course}/posts/{post}', function($course_id, Post $post){
-    auth()->user()->readPosts()->attach($post->id);
-    givePoint(new PostCompleted($post));
-    return response()->json(['ok' => 'ok']);
-})->name('posts.read');
 
 
 // Events
-Route::get('/event/create', 'EventController@create')->name('events.create');
-Route::post('/event','EventController@store')->name('events.store');
 Route::get('/event/{event}','EventController@show')->name('events.show');
-Route::get('/event/{event}/edit','EventController@edit')->name('events.edit');
-Route::put('/event/{event}','EventController@update')->name('events.update');
-Route::get('/event/delete/{event}','EventController@destroy')->name('events.destroy');
 Route::get('/event','EventController@index')->name('events.index');
 
+//ckeditor
 Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
+
 //instructor
 Route::get('/instructors', 'InstructorController@index')->name('instructors.index');
 Route::get('/instructors/{instructor}', 'InstructorController@show')->name('instructors.show');
@@ -80,32 +48,15 @@ Route::get('/instructors/{instructor}', 'InstructorController@show')->name('inst
 
 Route::post('ckeditor/image_upload', 'CKEditorController@upload')->name('upload');
 
-//Profile
-Route::get('/profile/{user}','UserController@show')->name('user.show');
-Route::get('/profile/{user}/edit','UserController@edit')->name('user.edit');
-Route::put('/profile/{user}','UserController@update')->name('user.update');
 
 
 Route::get('/calender', function () { return view('calender'); });
 Route::get('/courses-posts', function () { return view('courses_posts'); });
-// Route::get('/course', function () { return view('course'); });
-Route::get('/teachers', function () { return view('teachers'); });
-Route::get('/teacher-details', function () { return view('teacher_details'); });
-// Route::get('/event', function () { return view('event'); });
-Route::get('/faq', function () { return view('faq'); });
-// Route::get('/event-details', function () { return view('event_details'); });
+Route::get('/faq', function () { return view('faq'); })->name('faq');
 Route::get('/timetable', function () { return view('timetable'); });
-Route::get('/users', function () { return view('auth/user'); })->name('users');
 
-//Dashboard
-Route::get('/dashboard/{slug}','DashboardController@index')->name('dashboard')->where("slug","instructor|parent|student");
-Route::get('/dashboard/{slug}/students',function(){return view('dashboard.dashboard_students');})->name('dashboard.students');
-Route::get('/dashboard/parent/create','DashboardController@create')->name('dashboard.create');
-Route::get('/dashboard/parent/progress','DashboardController@progress')->name('dashboard.progress');
-Route::post('/dashboard/parent', 'DashboardController@store')->name('dashboard.store');
-Route::get('/dashboard/parent/{id}','DashboardController@login')->name('dashboard.login');
-Route::get('/dashboard/{slug}/events','DashboardController@instructor_events')->name('dashboard.events');
-Route::get('/dashboard/student', function () { return view('dashboard.student'); })->name('dashboard.student');
+//home page
+Route::get('/', 'HomeController@index')->name('home');
 
 
 // Auth::routes();
@@ -135,23 +86,75 @@ Route::group(['middleware' => ['web']], function() {
     Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+//users
+    Route::get('/users', function () { return view('auth/user'); })->name('users');
+
+//login with google and facebook
+    Route::get('/redirect/{driver}', 'Auth\LoginController@redirectToProvider')->name('login.provider');
+    Route::get('/home/{provider}', 'Auth\LoginController@handleProviderCallback')->name('login.access');
+
+    Route::get('/redirect/{driver}', 'Auth\RegisterController@redirectToProvider')->name('register.provider');
+    Route::get('/home/{provider}', 'Auth\RegisterController@handleProviderCallback')->name('register.access');
+
 });
 
-//home page
-Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/redirect/{driver}', 'Auth\LoginController@redirectToProvider')->name('login.provider');
-Route::get('/home/{provider}', 'Auth\LoginController@handleProviderCallback')->name('login.access');
 
-Route::get('/redirect/{driver}', 'Auth\RegisterController@redirectToProvider')->name('register.provider');
-Route::get('/home/{provider}', 'Auth\RegisterController@handleProviderCallback')->name('register.access');
+Route::group(['middleware' => ['auth','verified']], function() {
+    
+    //Dashboard
+    Route::get('/dashboard/{slug}','DashboardController@index')->name('dashboard')->where("slug","instructor|parent|student");
+    Route::get('/dashboard/{slug}/students',function(){return view('dashboard.dashboard_students');})->name('dashboard.students');
+    Route::get('/dashboard/parent/create','DashboardController@create')->name('dashboard.create');
+    Route::get('/dashboard/parent/progress','DashboardController@progress')->name('dashboard.progress');
+    Route::post('/dashboard/parent', 'DashboardController@store')->name('dashboard.store');
+    Route::get('/dashboard/parent/{id}','DashboardController@login')->name('dashboard.login');
+    Route::get('/dashboard/instructor/events','DashboardController@instructor_events')->name('dashboard.events');
+    Route::get('/dashboard/student', function () { return view('dashboard.student'); })->name('dashboard.student');
+
+    //Profile
+    Route::get('/profile/{user}','UserController@show')->name('user.show');
+    Route::get('/profile/{user}/edit','UserController@edit')->name('user.edit');
+    Route::put('/profile/{user}','UserController@update')->name('user.update');
+
+    //instructor rate 
+    Route::post('instructor-rate', 'InstructorController@rateInstructor')->name('instructors.rate')->middleware('role:parent|student|admin');
+
+    //student enrollment 
+    Route::post('/courses/{course}/enroll', 'CourseController@enroll')->name('courses.enroll')->middleware('role:student');
+
+});
+
+
+Route::group(['middleware' => ['auth','verified','role:admin|instructor']], function() {
+    
+    //event
+    Route::get('/event/create', 'EventController@create')->name('events.create');
+    Route::post('/event','EventController@store')->name('events.store');
+    Route::get('/event/{event}/edit','EventController@edit')->name('events.edit');
+    Route::put('/event/{event}','EventController@update')->name('events.update');
+    Route::get('/event/delete/{event}','EventController@destroy')->name('events.destroy');
+
+    //post
+    Route::get('/courses/{course}/posts/create', 'PostController@create')->name('posts.create');
+    Route::post('/courses/{course}/posts', 'PostController@store')->name('posts.store');
+    Route::get('/courses/{course}/posts/{post}/edit', 'PostController@edit')->name('posts.edit');
+    Route::put('/courses/{course}/posts/{post}', 'PostController@update')->name('posts.update');
+    Route::delete('/courses/{course}/posts/{post}', 'PostController@destroy')->name('posts.destroy');
+    Route::post('/courses/{course}/posts/{post}', function($course_id, Post $post){
+        auth()->user()->readPosts()->attach($post->id);
+        givePoint(new PostCompleted($post));
+        return response()->json(['ok' => 'ok']);
+    })->name('posts.read');
+
+});
+
 
 // Route::get('/banned',function(){ return view('banned');});
 //Rate
 Route::post('post-rate', 'PostController@ratePost')->middleware('auth')->name('posts.rate');
 Route::post('course-rate', 'CourseController@rateCourse')->middleware('auth')->name('courses.rate');
 
-Route::post('instructor-rate', 'InstructorController@rateInstructor')->middleware('auth')->name('instructors.rate');
 
 
 //contact-us
